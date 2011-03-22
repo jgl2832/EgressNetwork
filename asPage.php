ca1781c5e86721a85100d457085ae9f7aab530c7
Jake <jgl2832@gmail.com>
2011-03-22 16:47:41 -0400

    asPage updated to access mysql

--- asPage.php
+++ asPage.php
@@ -5,6 +5,34 @@
 <body>
 <h1>Autonomous System <?php echo $_GET['as']; ?></h1>
 <?php
+$dbhost = 'hansonbros.ece.mcgill.ca';
+$dbuser = 'bgp';
+$dbpass = 'bgppasswd';
+$conn = mysql_connect($dbhost,$dbuser,$dbpass) 
+	or die('Error Connecting to mySQL');
+$dbname = 'egressNetworkProj';
+mysql_select_db($dbname);
+
+$result = mysql_query('CALL get1RouteStrByASN('.$_GET['as'].')')
+	or die(mysql_error());
+
+echo 'Route(s) From McGill:<br><ul>';
+
+while($row = mysql_fetch_array($result)) {
+	$token = strtok($row['path'],' ');
+	echo '<li>';
+	while ($token != false) {
+		echo '<a href="asPage.php?as='.$token.'">'.$token.'</a> ';
+		//echo $token.' ';
+		$token = strtok(" ");
+	}
+	echo '</li><br>';
+}
+echo '</ul';
+?>
+
+<p>Whois info:</p>
+<?php
 	$arr = array();
 	exec("whois as".$_GET['as'],$result);
 	foreach($result as $i) {
