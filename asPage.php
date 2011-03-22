d30098d79649d4b54294ee3ce50c77a19655f1ce
Jake <jgl2832@gmail.com>
2011-03-22 18:26:38 -0400

    Added route list and map

--- asPage.php
+++ asPage.php
@@ -13,12 +13,13 @@ $conn = mysql_connect($dbhost,$dbuser,$dbpass)
 $dbname = 'egressNetworkProj';
 mysql_select_db($dbname);
 
-$result = mysql_query('CALL get1RouteStrByASN('.$_GET['as'].')')
+$result = mysql_query('CALL getRouteStrByASN('.$_GET['as'].')')
 	or die(mysql_error());
 
 echo 'Route(s) From McGill:<br><ul>';
 
 while($row = mysql_fetch_array($result)) {
+	$last = $row['path'];
 	$token = strtok($row['path'],' ');
 	echo '<li>';
 	while ($token != false) {
@@ -28,19 +29,65 @@ while($row = mysql_fetch_array($result)) {
 	}
 	echo '</li><br>';
 }
-echo '</ul';
+echo '</ul>';
 ?>
 
-<p>Whois info:</p>
+
 <?php
+	function getAddress($asid) {
+		exec("whois as".$asid,$asResult);
+		$queryString = "";
+		foreach($asResult as $i) {
+			
+			if (strncmp(strtolower($i), 'address:',8) == 0) {
+				$addString = str_replace(" ","+",trim(substr($i, 8)));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'city:',5) == 0) {
+				$addString = str_replace(" ","+",trim(substr($i,5) ));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'stateprov:',10) == 0) {
+				$addString = str_replace(" ","+",trim(substr($i,10)));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'country:',8) == 0) {
+				$addString = str_replace(" ","+",trim(substr($i,8)));
+				$queryString = $queryString."".$addString;
+			}	
+		}
+		return $queryString;
+	}
+
+	echo '<img src="http://maps.google.com/maps/api/staticmap?size=500x200';
+	$token = strtok($last, ' ');
+
+	while ($token != false) {
+		echo '&markers=size:large|color:green|'.getAddress($token);
+		$token = strtok(' ');
+	}
+/*
+	echo '&path=color:0xff0000ff|weight:5';
+	$token = strtok($last, ' ');
+	while ($token != false) {
+		echo '|'.getAddress($token);
+	}
+*/
+	echo '&sensor=false" />';
+
 	$arr = array();
 	exec("whois as".$_GET['as'],$result);
+
+	
+?>
+<p>Whois info:</p>
+<?php
 	foreach($result as $i) {
 		if (substr($i,0,1) != "#") {
 			print $i."<br>";
 		}
 	}
 ?>
-<p>test</p>
+
 </body>
 
