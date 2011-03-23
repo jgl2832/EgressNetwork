1f1ac6387a661a4b01a67e335fca55501b2d3816
Frederic Weigand Warr <fwwarr@gmail.com>
2011-03-23 17:50:04 -0400

    Added flot library for drawing graphs. Added route length distribution graph.

--- bgp.php
+++ bgp.php
@@ -1,6 +1,8 @@
 <html>
 <head>
-<title>BGP Statistics</title>
+	<title>BGP Statistics</title>
+	<script language="javascript" type="text/javascript" src="flot/jquery.js"></script>
+    <script language="javascript" type="text/javascript" src="flot/jquery.flot.js"></script>
 </head>
 <body>
 <h1>BGP Statistics</h1>
@@ -37,16 +39,45 @@ $result = mysql_query($query)
 	or die("Query failed: " . mysql_error() . "<br /> Query: " . $query);
 
 echo 'Route Length Distribution:<br><ul>';
-
+$a = array();
+$i = 0;
+$s = '';
 while($row = mysql_fetch_assoc($result)) {
 	
-	echo $row['length'].': '.$row['count'].'<br />';
+	//echo $row['length'].': '.$row['count'].'<br />';
+	$a[$i] = $row['length'].': '.$row['count'];
+	$i = $i+1;
+	$s = $s.$row['length'].':'.$row['count'].' ';
 }
 echo '</ul>';
 mysql_close($conn);
 
 ?>
 
+<div id="placeholder" style="width:600px;height:300px;"></div>
 
+<script type="text/javascript">
+$(function () {
+	var str = "<?php echo $s; ?>"
+    var d1 = str.split(' ');
+    for (var i = 0; i < d1.length; i ++)
+        d1[i] = d1[i].split(':');
+
+    //var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+    // a null signifies separate line segments
+    //var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+    
+    $.plot($("#placeholder"), [ d1 ]);
+});
+</script>
+<br />
+Values: <br /><ul>
+<?php 
+foreach($a as $j)
+	echo '<li>'.$j.'</li>';
+
+?>
+</ul>
 </body>
 </html>
