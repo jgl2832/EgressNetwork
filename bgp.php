efb540627aaa1bb8d8c7df36f32093a103363ec9
Frederic Weigand Warr <fwwarr@gmail.com>
2011-03-23 13:08:23 -0400

    Improved performance for stats page

--- bgp.php
+++ bgp.php
@@ -14,37 +14,18 @@ $conn = mysql_connect($dbhost,$dbuser,$dbpass, true, 65536)
 $dbname = 'egressNetworkProj';
 mysql_select_db($dbname);
 $datetime = date( 'Y-m-d H:i:s');
-$query = 'CALL getDistinctRouteCount(\''.$datetime.'\')';
+$query = 'SELECT count, avgLength FROM RouteStatsHistory ORDER BY -date LIMIT 1;';
 $result = mysql_query($query)
 	or die("Query failed: " . mysql_error() . "<br /> Query: " . $query);
 
-echo 'Number of distinct routes:<br><ul>';
-
 while($row = mysql_fetch_assoc($result)) {
-	
-	echo $row['count'].'<br />';
+
+	echo 'Number of distinct routes: ';
+	echo $row['count'].'<br /><br />';
+	echo 'Average BGP Route Length: ';
+	echo $row['avgLength'].'<br /><br />';
 }
-echo '</ul>';
 mysql_close($conn);
-
-$conn = mysql_connect($dbhost,$dbuser,$dbpass, true, 65536) 
-	or die('Error Connecting to mySQL');
-$dbname = 'egressNetworkProj';
-mysql_select_db($dbname);
-$datetime = date( 'Y-m-d H:i:s');
-$query = 'CALL getAverageLength(\''.$datetime.'\')';
-$result = mysql_query($query)
-	or die("Query failed: " . mysql_error() . "<br /> Query: " . $query);
-
-echo 'Average BGP Route Length:<br><ul>';
-
-while($row = mysql_fetch_assoc($result)) {
-	
-	echo $row['length'].'<br />';
-}
-echo '</ul>';
-mysql_close($conn);
-
 
 $conn = mysql_connect($dbhost,$dbuser,$dbpass, true, 65536) 
 	or die('Error Connecting to mySQL');
