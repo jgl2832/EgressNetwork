7f4b0dd5719f36b009f0aa7b17f655a4a01e4e87
Frederic Weigand Warr <fwwarr@gmail.com>
2011-03-24 02:20:05 -0400

    New home page at index.php.

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<html>
+	<head>
+		<title>Egress Network Monitoring</title>
+	</head>
+	<body>
+	<?php include("nav.php"); ?>
+	
+	<div id="content" style="margin-left:230px;" >
+		<h1>Welcome to the McGill University Egress Network Monitoring Utility Page! (MUENMUP)</h1>
+		
+		<br />
+		<br />
+		This web site displays information about all BGP Routes from McGill.<br />
+		<br />
+		
+		
+		It has been created by the Software Design Project Group 33 composed of
+		<ul>
+		<li>Jake Levine</li>
+		<li>Eubene Sa</li>
+		<li>Frederic Weigand Warr</li>
+		</ul>
+		Under the supervision of Professor Michael Rabbat and with help from Zhe Yao.
+		
+	</div>
+	</body>
+</html>
