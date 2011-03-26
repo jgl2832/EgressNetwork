db2adcc80c9412579a9107bcbb2ae2add644da8b
Eubene Sa <eubenesa@gmail.com>
2011-03-26 16:57:44 -0400

    a

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
-
 <html>
 	<head>
-		<title>Egress Network Monitoring</title>
+		<title>Egress Network Monitoring and Analytics</title>
 	</head>
 	<body>
 	<?php include("nav.php"); ?>
-	
 	<div id="content" style="margin-left:230px;" >
-		<h1>Welcome to the McGill University Egress Network Monitoring Utility Page! (MUENMUP)</h1>
-		
+		<h1>Welcome to the McGill University Egress Network Monitoring Utility Page!</h1>
 		<br />
 		<br />
-		This web site displays information about all BGP Routes from McGill.<br />
+		This website displays information about all BGP Routes from McGill.<br />
 		<br />
-		
-		
-		It has been created by the Software Design Project Group 33 composed of
+		Created by the Software Engineering Design Project Group 33:
 		<ul>
 		<li>Jake Levine</li>
 		<li>Eubene Sa</li>
 		<li>Frederic Weigand Warr</li>
 		</ul>
-		Under the supervision of Professor Michael Rabbat and with help from Zhe Yao.
-		
+		Under the supervision of Professor Michael Rabbat and with help from Ph.D. student Zhe Yao
 	</div>
 	</body>
 </html>
