deb75e806372b7c5e3e2dc1d167d60cb6e54e596
Jake <jgl2832@gmail.com>
2011-03-26 17:28:44 -0400

    made map work better

--- route.php
+++ route.php
@@ -1,15 +1,31 @@
 <html>
 <head>
 <title>Route details</title>
-</head>
-<body>
-
-<?php include("nav.php"); ?>
-
-<div id="content" style="margin-left:230px;" >
-
-<h1>Route Details</h1>
 <?php
+	function getAddress($asid) {
+		exec("whois as".$asid,$asResult);
+		$queryString = "";
+		foreach($asResult as $i) {
+			
+			if (strncmp(strtolower($i), 'address:',8) == 0) {
+				$addString = trim(substr($i, 8));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'city:',5) == 0) {
+				$addString = trim(substr($i,5));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'stateprov:',10) == 0) {
+				$addString = trim(substr($i,10));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'country:',8) == 0) {
+				$addString = trim(substr($i,8));
+				$queryString = $queryString."".$addString;
+			}	
+		}
+		return $queryString;
+	}
 $dbhost = 'hansonbros.ece.mcgill.ca';
 $dbuser = 'bgp';
 $dbpass = 'bgppasswd';
@@ -20,6 +36,78 @@ $conn = mysql_connect($dbhost,$dbuser,$dbpass, true, 65536)
 	or die('Error Connecting to mySQL');
 mysql_select_db($dbname);
 $datetime = date( 'Y-m-d H:i:s');
+$query = 'CALL getRoute('.$_GET['id'].')';
+$result = mysql_query($query)
+	or die("Query failed: " . mysql_error() . "<br /> Query: " . $query);
+?>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false">
+</script>
+<script type="text/javascript">
+  var map;
+  var geocoder;
+  var bounds;
+    var latlng = new google.maps.LatLng(73, 43);
+    var bounds = new google.maps.LatLngBounds();
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+
+<?php
+
+	while($row = mysql_fetch_assoc($result)) {
+		$last = $row['path'];
+	}
+	$token = strtok($last, ' ');
+	while ($token != false) {
+		echo 'codeAddress("'.getAddress($token).'");';
+		$token = strtok(' ');
+	}
+?>
+  }
+function codeAddress(address) {
+	
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+	bounds.extend(results[0].geometry.location);
+        map.fitBounds(bounds);
+        var marker = new google.maps.Marker({
+            map: map, 
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+	
+  }
+
+</script>
+
+</head>
+<body onload=initialize()>
+
+
+<?php include("nav.php"); ?>
+
+<div id="content" style="margin-left:230px;" >
+
+<h1>Route Details</h1>
+<?php
+
+mysql_close($conn);
+
+
+//Agregator information
+$conn = mysql_connect($dbhost,$dbuser,$dbpass, true, 65536) 
+	or die('Error Connecting to mySQL');
+mysql_select_db($dbname);
 $query = 'CALL getRoute('.$_GET['id'].')';
 $result = mysql_query($query)
 	or die("Query failed: " . mysql_error() . "<br /> Query: " . $query);
@@ -72,52 +160,10 @@ while($row = mysql_fetch_assoc($result)) {
 }
 echo '</ul><br />';
 mysql_close($conn);
+?>
 
-
-
-
-
-
-	function getAddress($asid) {
-		exec("whois as".$asid,$asResult);
-		$queryString = "";
-		foreach($asResult as $i) {
-			
-			if (strncmp(strtolower($i), 'address:',8) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i, 8)));
-				$queryString = $queryString."".$addString.",";
-			}
-			if (strncmp(strtolower($i),'city:',5) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i,5) ));
-				$queryString = $queryString."".$addString.",";
-			}
-			if (strncmp(strtolower($i),'stateprov:',10) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i,10)));
-				$queryString = $queryString."".$addString.",";
-			}
-			if (strncmp(strtolower($i),'country:',8) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i,8)));
-				$queryString = $queryString."".$addString;
-			}	
-		}
-		return $queryString;
-	}
-	echo 'Route Map<br />';
-	echo '<img src="http://maps.google.com/maps/api/staticmap?size=500x200';
-	$token = strtok($last, ' ');
-
-	while ($token != false) {
-		echo '&markers=size:large|color:green|'.getAddress($token);
-		$token = strtok(' ');
-	}
-/*
-	echo '&path=color:0xff0000ff|weight:5';
-	$token = strtok($last, ' ');
-	while ($token != false) {
-		echo '|'.getAddress($token);
-	}
-*/
-	echo '&sensor=false" />';
+	<div id="map_canvas" style="width: 500px; height: 200px"></div>	
+<?php
 
 	$arr = array();
 	exec("whois as".$_GET['as'],$result);
