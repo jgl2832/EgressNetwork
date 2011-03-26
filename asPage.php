af65b54de4946874861de8ddc97532985145ef8e
Jake <jgl2832@gmail.com>
2011-03-26 18:32:09 -0400

    changed asPage.php

--- asPage.php
+++ asPage.php
@@ -1,15 +1,32 @@
 <html>
 <head>
 <title>Autonomous System <?php echo $_GET['as']; ?></title>
-</head>
-<body>
-
-<?php include("nav.php"); ?>
-
-<div id="content" style="margin-left:230px;" >
-
-<h1>Autonomous System <?php echo $_GET['as']; ?></h1>
 <?php
+	function getAddress($asid) {
+		exec("whois as".$asid,$asResult);
+		$queryString = "";
+		foreach($asResult as $i) {
+			
+			if (strncmp(strtolower($i), 'address:',8) == 0) {
+				$addString = trim(substr($i, 8));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'city:',5) == 0) {
+				$addString = trim(substr($i,5));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'stateprov:',10) == 0) {
+				$addString = trim(substr($i,10));
+				$queryString = $queryString."".$addString.",";
+			}
+			if (strncmp(strtolower($i),'country:',8) == 0) {
+				$addString = trim(substr($i,8));
+				$queryString = $queryString."".$addString;
+			}	
+		}
+		return $queryString;
+	}
+
 $dbhost = 'hansonbros.ece.mcgill.ca';
 $dbuser = 'bgp';
 $dbpass = 'bgppasswd';
@@ -20,6 +37,61 @@ mysql_select_db($dbname);
 
 $result = mysql_query('CALL getRouteStrByASN('.$_GET['as'].')')
 	or die(mysql_error());
+?>
+
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false">
+</script>
+<script type="text/javascript">
+  var map;
+  var geocoder;
+    var latlng = new google.maps.LatLng(73, 43);
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+   
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+
+<?php
+
+	echo 'codeAddress("'.getAddress($_GET['as']).'","'.$_GET['as'].'");';
+
+?>
+
+  }
+function codeAddress(address, id) {
+	
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+
+        var marker = new google.maps.Marker({
+            map: map, 
+	    cursor: id,
+	    title: id,
+            position: results[0].geometry.location
+        });
+      } else {
+		document.getElementById("map_canvas").style.visibility = 'hidden'; 
+	}
+    });
+	
+  }
+
+</script>
+</head>
+<body onload=initialize()>
+
+<?php include("nav.php"); ?>
+
+<div id="content" style="margin-left:230px;" >
+
+<h1>Autonomous System <?php echo $_GET['as']; ?></h1>
+<?php
 
 $row = mysql_fetch_array($result);
 if($row == '') { 
@@ -45,48 +117,8 @@ else {
 echo '</ul>';
 ?>
 
-
+<div id="map_canvas" style="width: 500px; height: 200px"></div>	
 <?php
-	function getAddress($asid) {
-		exec("whois as".$asid,$asResult);
-		$queryString = "";
-		foreach($asResult as $i) {
-			
-			if (strncmp(strtolower($i), 'address:',8) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i, 8)));
-				$queryString = $queryString."".$addString.",";
-			}
-			if (strncmp(strtolower($i),'city:',5) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i,5) ));
-				$queryString = $queryString."".$addString.",";
-			}
-			if (strncmp(strtolower($i),'stateprov:',10) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i,10)));
-				$queryString = $queryString."".$addString.",";
-			}
-			if (strncmp(strtolower($i),'country:',8) == 0) {
-				$addString = str_replace(" ","+",trim(substr($i,8)));
-				$queryString = $queryString."".$addString;
-			}	
-		}
-		return $queryString;
-	}
-
-	echo '<img src="http://maps.google.com/maps/api/staticmap?size=500x200';
-	$token = strtok($last, ' ');
-
-	while ($token != false) {
-		echo '&markers=size:large|color:green|'.getAddress($token);
-		$token = strtok(' ');
-	}
-/*
-	echo '&path=color:0xff0000ff|weight:5';
-	$token = strtok($last, ' ');
-	while ($token != false) {
-		echo '|'.getAddress($token);
-	}
-*/
-	echo '&sensor=false" />';
 
 	$arr = array();
 	exec("whois as".$_GET['as'],$result);
