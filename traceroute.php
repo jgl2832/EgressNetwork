15cdfd6cdcc01e68d7b4901dab7eb534e6ac92e9
Jake <jgl2832@gmail.com>
2011-03-27 18:11:11 -0400

    added IP links on traceroute page

--- traceroute.php
+++ traceroute.php
@@ -15,9 +15,24 @@
 
 <p>Traceroute info:</p>
 <?php
+	$open = '(';
+	$close = ')';
         foreach($result as $i) {
                 if (substr($i,0,1) != "#") {
-                        print $i."<br>";
+			$pos1 = strpos($i,$open);
+			$pos2 = strpos($i,$close);
+			if (($pos1 == false) || ($pos2 == false)) {
+				echo $i;
+			} else {
+				echo substr($i,0,$pos1 + 1);
+				echo '<a href="ip.php?ip=';
+				echo substr($i,$pos1 + 1,$pos2-$pos1 - 1);
+				echo '">';
+				echo substr($i,$pos1 + 1,$pos2-$pos1 - 1);
+				echo '</a>';
+				echo substr($i,$pos2);
+			}
+			print "<br>";
                 }
         }
 ?>
